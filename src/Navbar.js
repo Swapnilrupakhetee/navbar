a75c2c4b00fbd9afd869e574932eab35357787ed
Swapnil <rupakheteeswapnil@gmail.com>
2023-10-18 21:07:22 +0545

    3rd commit

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,21 +1,28 @@
 import {FaBars,FaTimes} from 'react-icons/fa';
 import React from 'react'
+import { useRef } from 'react';
+import'../src/Navbar.css';
 
 function Navbar() {
+    const navRef = useRef();
+    const showNavbar = ()=>
+    {
+        navRef.current.classList.toggle('responsive_nav');
+    }
   return (
     <header>
         <h3>Logo</h3>
-        <nav>
+        <nav ref={navRef}>
             <a href='#'>Home</a>
             <a href='#'>Shop</a>
             <a href='#'>Contact</a>
             <a href='#'>About Me</a>
-            <button>
+            <button className='nav-btn nav-close-btn' onClick={showNavbar}>
                 <FaTimes/>
             </button>
 
         </nav>
-        <button>
+        <button className='nav-btn' onClick={showNavbar}>
             <FaBars/>
         </button>
 
